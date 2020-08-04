24588b3e19e6856c98cc9f94c1f23ac6b87d4a3e
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-04 12:51:07 -0500

    Created the Slug function to transform name to the proper format

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -93,6 +94,13 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// create bootcamp slug from the name
+
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
