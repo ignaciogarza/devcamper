dfcaa1518b4b1fc5459bee8d4eff6f5a9f32f774
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-04 12:37:59 -0500

    Middleware for error handling

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+const colors = require("colors");
 
 // Load ENV vars
 dotenv.config({ path: "./config/config.env" });
 
+// connect to DB
+connectDB();
+
+// route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+//
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan("development"));
 }
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Error middleware handler
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
