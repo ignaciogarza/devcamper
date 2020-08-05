5bbf2cbde522e97c5b2034b838ed44c6b6e8d653
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-04 22:44:16 -0500

    Seeder for courses

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotEnv.config({
 
 // load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -24,10 +25,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 // Import data into Database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data imported..".green.inverse);
     process.exit();
   } catch (error) {
@@ -39,6 +45,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data destroyed..".red.inverse);
     process.exit();
   } catch (error) {
