5c0baaec402d286c02bebe2f2591a2e02be52a36
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-05 00:29:01 -0500

    Update and Delete API routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -32,7 +32,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
   // select fields
   if (req.query.select) {
@@ -141,13 +141,16 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @Access      Private
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
+
   res.status(200).json({ success: true, data: {} });
 });
 
