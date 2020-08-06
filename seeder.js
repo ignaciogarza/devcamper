648cc31331c46a32139f5b9c5e0d95e2dc33ff56
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-06 15:52:32 -0500

    fix user create error (id)

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotEnv.config({
 // load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,11 +30,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
+
 // Import data into Database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log("Data imported..".green.inverse);
     process.exit();
   } catch (error) {
@@ -46,6 +52,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data destroyed..".red.inverse);
     process.exit();
   } catch (error) {
