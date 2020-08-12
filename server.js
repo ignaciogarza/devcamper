90eea3f13600e205e06eb132654d69e29dd9cff7
Ignacio Garza <ignacioagarza@gmail.com>
2020-08-12 11:28:25 -0500

    added security packages to avoid common atacks and limit security holes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 const colors = require("colors");
@@ -37,6 +43,29 @@ if (process.env.NODE_ENV === "development") {
 // File Uploading
 
 app.use(fileUpload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// set security header
+app.use(helmet());
+
+// Prevent XSS atacks
+app.use(xss());
+
+// Rate limitng
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable Cors
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
